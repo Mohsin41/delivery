1488f228e3b0375748cced67ba04f76294ad2d78
Mohsin <mohsinafzal29@gmail.com>
2021-06-22 11:48:18 +0200

    Lint the code

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -13,10 +13,11 @@ router.get('/initialize', async (req, res) => {
     weight: 5,
     deliveryType: 'normal',
   })
- await sample1.save()
-  const a=sample1.getDeliveryDate()
+  await sample1.save()
+  
+  //const a=sample1.getDeliveryDate()
 
-  console.log(`the date is:${a}`)
+  // console.log(`the date is:${a}`)
   
 
   res.sendStatus(204)
@@ -29,15 +30,17 @@ router.post('/',  async (req, res) => {
     
   }
 
-const addedProduct = await Product.create(productToAdd)
+  const addedProduct = await Product.create(productToAdd)
   res.send(addedProduct)
 })
 
+
+/*possible end point for storing deliverydate in backend with order model maybe
 router.post('/delivery', async (req, res) =>{
    const product = await Product.findById(req.params.Id)
    await product.getDeliveryDate(new Date())
 
   res.sendStatus(204) 
-})
+}) */
 module.exports = router
 
