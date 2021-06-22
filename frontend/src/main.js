a929da1d9dff960e35640898cf0acdb38552d094
Mohsin <mohsinafzal29@gmail.com>
2021-06-22 11:50:38 +0200

    Import sweetalert globally in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,16 @@ import router from './router'
 import store from './store'
 import 'normalize.css'
 import 'bootstrap'
+import VueSweetalert2 from 'vue-sweetalert2';
 
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
 /*createApp(App)
   .use(store)
   .use(router)
   .mount('#app')*/
-  Vue.config.productionTip = false
+Vue.config.productionTip = false
 
 async function main() {
   let storeInstance = await store()
